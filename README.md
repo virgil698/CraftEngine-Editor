34a55e82a126830f5eaeb3b695cf8e3939a2c36e
virgil698 <xiaokong23357@163.com>
2025-07-23 09:34:57 +0800

    首次提交代码

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
+![](resources/logo.ico)
 # CraftEngine-Editor
-CraftEngine-Editor
+
+## 简介
+CraftEngine-Editor是一个基于PyQt5的项目，用于快速创建和编辑基于CraftEngine插件的拓展包配置文件。
+
+## 特性
+- 快速创建新的插件配置文件
+- 编辑已有的插件配置文件
+- 实时预览配置文件编辑结果
+- 支持 ItemsAdder & Nexo 配置转换为 CraftEngine 配置
+- 支持插件配置文件的版本控制
+- 支持插件配置文件的备份和恢复
+- 支持插件配置文件的导入和导出
+
+## 项目结构
+```angular2html
+CraftEngine-Editor/
+├── app.py # 应用程序的入口
+├── ui/ # 存放UI文件的目录
+│ ├── main_window.ui # 主窗口的UI文件
+├── resources/ # 存放资源文件（如图像、图标等）
+│ ├── icon.png
+├── styles/ # 存放样式文件（如QSS等）
+│ ├── style.qss
+├── modules/ # 存放项目模块的目录
+│ ├── module1.py
+│ ├── module2.py
+└── README.md # 项目的说明文件
+```
+
+## 安装依赖
+```bash
+pip install -r requirements.txt
+```
+
+## 运行项目
+```bash
+python app.py
+```
--- app.py
+++ app.py
@@ -2,15 +2,26 @@ import sys
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-def window():
+
+class window(QWidget):
+   def __init__(self, parent = None):
+      super(window, self).__init__(parent)
+      self.resize(1024,600)
+      self.setWindowTitle("CraftEngine-Editor")
+      self.label = QLabel(self)
+      self.label.setText("Hello World")
+      font = QFont()
+      font.setFamily("Arial")
+      font.setPointSize(16)
+      self.label.setFont(font)
+      self.label.move(50,20)
+      self.setWindowIcon(QIcon('resources/logo.ico'))
+
+def main():
    app = QApplication(sys.argv)
-   w = QWidget()
-   b = QLabel(w)
-   b.setText("Hello World!")
-   w.setGeometry(100,100,200,50)
-   b.move(50,20)
-   w.setWindowTitle("PyQt5")
-   w.show()
+   ex = window()
+   ex.show()
    sys.exit(app.exec_())
+
 if __name__ == '__main__':
-   window()
+   main()
